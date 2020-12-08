7585c75a6a2bba1393c36c7cef4c8eb8ccae62ec
Hector Niñez J <desarrollo.perrosky@gmail.com>
2020-12-08 14:34:46 -0300

    Manejo de lectura OK

--- app/src/main/java/cl/perrosky/organizapp/IngresarProductoActivity.java
+++ app/src/main/java/cl/perrosky/organizapp/IngresarProductoActivity.java
@@ -1,14 +1,109 @@
 package cl.perrosky.organizapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class IngresarProductoActivity extends AppCompatActivity {
+
+    private static final int CODIGO_PERMISOS_CAMARA = 1;
+    private static final int CODIGO_INTENT = 2;
+
+    private boolean permisoCamaraConcedido = false;
+    private boolean permisoSolicitadoDesdeBoton = false;
+
+    private TextView tvCodigoLeido;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ingresar_producto);
+        verificarYPedirPermisosDeCamara();
+
+        ImageView btnEscanear = (ImageView) findViewById(R.id.btnEscanear);
+        tvCodigoLeido = findViewById(R.id.codigo);
+        btnEscanear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!permisoCamaraConcedido) {
+                    Toast.makeText(IngresarProductoActivity.this, "Por favor permite que la app acceda a la cámara", Toast.LENGTH_SHORT).show();
+                    permisoSolicitadoDesdeBoton = true;
+                    verificarYPedirPermisosDeCamara();
+                    return;
+                }
+                escanear();
+            }
+        });
+    }
+
+    private void escanear() {
+        Intent i = new Intent(IngresarProductoActivity.this, EscanearActivity.class);
+        startActivityForResult(i, CODIGO_INTENT);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CODIGO_INTENT) {
+            if (resultCode == Activity.RESULT_OK) {
+                if (data != null) {
+                    String codigo = data.getStringExtra("codigo");
+                    tvCodigoLeido.setText(codigo);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case CODIGO_PERMISOS_CAMARA:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Escanear directamten solo si fue pedido desde el botón
+                    if (permisoSolicitadoDesdeBoton) {
+                        escanear();
+                    }
+                    permisoCamaraConcedido = true;
+                } else {
+                    permisoDeCamaraDenegado();
+                }
+                break;
+        }
+    }
+
+    private void verificarYPedirPermisosDeCamara() {
+        int estadoDePermiso = ContextCompat.checkSelfPermission(IngresarProductoActivity.this, Manifest.permission.CAMERA);
+        if (estadoDePermiso == PackageManager.PERMISSION_GRANTED) {
+            // En caso de que haya dado permisos ponemos la bandera en true
+            // y llamar al método
+            permisoCamaraConcedido = true;
+        } else {
+            // Si no, pedimos permisos. Ahora mira onRequestPermissionsResult
+            ActivityCompat.requestPermissions(IngresarProductoActivity.this,
+                    new String[]{Manifest.permission.CAMERA},
+                    CODIGO_PERMISOS_CAMARA);
+        }
+    }
+
+
+    private void permisoDeCamaraDenegado() {
+        // Esto se llama cuando el usuario hace click en "Denegar" o
+        // cuando lo denegó anteriormente
+        Toast.makeText(IngresarProductoActivity.this, "No puedes escanear si no das permiso", Toast.LENGTH_SHORT).show();
     }
 }
